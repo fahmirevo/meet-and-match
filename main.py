059f217dce19aa2578821cd25256f708d5cd9c53
fahmi <fahmi.velosa1997@gmail.com>
2018-11-16 20:21:23 +0700

    restructure loop

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ torch.set_default_tensor_type('torch.cuda.FloatTensor')
 cuda = torch.device('cuda')
 
 n_images = 4031
-n_epochs = 50
+n_epochs = 20
 n_features = 10
 n_labels = 20
 n_samples = 2
-epoch_loop = n_images // (n_samples * n_labels + n_labels)
-epochs = n_epochs * epoch_loop
+n_batches = n_images // (n_samples * n_labels + n_labels)
+# epochs = n_epochs * epoch_loop
 
 
 reader = readers.ImageReader(n_labels, n_samples)
@@ -39,49 +39,51 @@ optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
 
 err = []
-for i in range(epochs):
-    optimizer.zero_grad()
-    references = torch.zeros(n_labels, n_features)
-    for j in range(n_labels):
-        _, im = reader.read(j)
-        im = im.cuda()
-        references[j] = hw_forward_pass(net, im)
+for epoch in range(n_epochs):
+    for i in range(n_batches):
+        optimizer.zero_grad()
+        references = torch.zeros(n_labels, n_features)
+        for j in range(n_labels):
+            _, im = reader.read(j)
+            im = im.cuda()
+            references[j] = hw_forward_pass(net, im)
 
-    # variance = references.std(1)
-    # output = 1 / (variance + 10 ** -8)
-    # loss = variance_criterion(torch.zeros(n_labels).cuda(), output)
-    # print('-----------------------------------------------------')
-    # print('distuingish loss : ', loss)
-    # loss.backward(retain_graph=True)
-    # optimizer.step()
+        # variance = references.std(1)
+        # output = 1 / (variance + 10 ** -8)
+        # loss = variance_criterion(torch.zeros(n_labels).cuda(), output)
+        # print('-----------------------------------------------------')
+        # print('distuingish loss : ', loss)
+        # loss.backward(retain_graph=True)
+        # optimizer.step()
 
-    out = reader.read()
-    total_loss = None
-    while out:
-        label, im = out
-        im = im.cuda()
-        feature = hw_forward_pass(net, im, squeeze=False)
-        target = torch.unsqueeze(references[label], 0)
-        opposition = references[torch.arange(n_labels) != label]
-        # reference = torch.unsqueeze(references[label], 0)
-        # target = torch.ones(n_labels) * -1
-        # target[i] = 1
-        # degree_loss = similarity_criterion(reference, feature, torch.Tensor([1]).cuda())
-        # degree_loss = similarity_criterion(references, feature, target.cuda())
-        # value_loss = value_criterion(reference, feature)
-        # loss = degree_loss + value_loss
-        loss = criterion(target, feature) / criterion(opposition, feature)
-        # print('identification loss : ', loss)
-        if total_loss is None:
-            total_loss = loss
-        else:
-            total_loss += loss.data[0]
         out = reader.read()
-    print(i, 'epoch loss : ', total_loss)
-    optimizer.zero_grad()
-    total_loss.backward()
-    optimizer.step()
-    err.append(float(total_loss))
+        total_loss = None
+        while out:
+            label, im = out
+            im = im.cuda()
+            feature = hw_forward_pass(net, im, squeeze=False)
+            target = torch.unsqueeze(references[label], 0)
+            opposition = references[torch.arange(n_labels) != label]
+            # reference = torch.unsqueeze(references[label], 0)
+            # target = torch.ones(n_labels) * -1
+            # target[i] = 1
+            # degree_loss = similarity_criterion(reference, feature, torch.Tensor([1]).cuda())
+            # degree_loss = similarity_criterion(references, feature, target.cuda())
+            # value_loss = value_criterion(reference, feature)
+            # loss = degree_loss + value_loss
+            loss = criterion(target, feature) / criterion(opposition, feature)
+            # print('identification loss : ', loss)
+            if total_loss is None:
+                total_loss = loss
+            else:
+                # total_loss += loss.data[0]
+                total_loss += loss.item()
+            out = reader.read()
+        print(i, 'epoch loss : ', total_loss)
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+        err.append(float(total_loss))
 
 with open('err.pickle', 'wb') as f:
     pickle.dump(err, f)
